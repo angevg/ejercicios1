e5e187682fe58d2a6330c032d4afc4eca9a4279c
angevg <aavg8@hotmail.com>
2019-11-02 21:13:54 -0400

    1er commit

---
+++ script1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <m
+    eta charset="utf-8">
+    <title>script1</title>
+    <style media="screen">
+
+    </style>
+
+  </head>
+  <body>
+<?php
+$nuevaURL = 'https://www.youtube.com';
+header('Location: '. $nuevaURL);
+?>
+
+    <header>
+    </header>
+
+  </body>
+</html>
